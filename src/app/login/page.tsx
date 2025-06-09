c8d55c3722eb37c4432c3779af100ef577447fb6
tomivillarreal <tomasvillarreal2003@outlook.com>
2025-06-09 20:12:53 -0300

    feat: Add LoginPage component with Web3Auth connection logic

---
+++ src/app/login/page.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { useWeb3AuthConnect } from "@web3auth/modal/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function LoginPage() {
+    const { connect, isConnected} = useWeb3AuthConnect();
+    const router = useRouter();
+    // Connect immediately when component mounts
+    useEffect(() => {
+        setTimeout(() => {
+            connect();
+        }, 1000);
+    }, []);
+
+
+    useEffect(() => {
+        if (isConnected) {
+            console.log("✅ User logged in");
+            router.push("/home"); // Redirect to dashboard after login
+        }
+    }, [isConnected]);
+
+    return (
+        <div className="container">
+            <h1 className="title">
+                Login
+            </h1>
+        </div>
+    );
+}
+
