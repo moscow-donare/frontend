6e7a632bfef64df77cf986420613f44f2f72cf5e
Nicolas Ludueña <nicolascluduena@gmail.com>
2025-06-09 18:26:44 -0300

    feat: Update provider and styles for Web3Auth integration and improved UI

--- src/app/components/provider.tsx
+++ src/app/components/provider.tsx
@@ -11,29 +11,45 @@ import React from "react";
 // IMP END - Setup Wagmi Provider
 
 // IMP START - Dashboard Registration
-const clientId = process.env.WEB3AUTH_CLIENT_ID ?? ''; // get from https://dashboard.web3auth.io
+const clientId = process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID ?? ""; // get from https://dashboard.web3auth.io
 // IMP END - Dashboard Registration
 
 // IMP START - Setup Wagmi Provider
 const queryClient = new QueryClient();
 // IMP END - Setup Wagmi Provider
- 
+
 // IMP START - Config
 const web3AuthContextConfig: Web3AuthContextConfig = {
-    web3AuthOptions: {
-      clientId,
-      web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
-        // IMP START - SSR
-      ssr: true,
-      // IMP END - SSR
-    }
-  };
-// IMP END - Config
+  web3AuthOptions: {
+    clientId,
+    web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
+    // IMP START - SSR
+    ssr: true,
+    chains: [
+      {
+        chainNamespace: "eip155",
+        chainId: "0x13882", // hexadecimal de 80002
+        displayName: "Polygon Amoy",
+        tickerName: "MATIC",
+        ticker: "MATIC",
+        decimals: 18,
+        rpcTarget: "https://rpc-amoy.polygon.technology",
+        blockExplorerUrl: "https://amoy.polygonscan.com",
+        logo: "", // opcional
+      },
+    ],
+  },
+};
 
 // IMP START - SSR
-export default function Provider({ children, web3authInitialState }: 
-  { children: React.ReactNode, web3authInitialState: IWeb3AuthState | undefined }) {
-// IMP END - SSR
+export default function Provider({
+  children,
+  web3authInitialState,
+}: {
+  children: React.ReactNode;
+  web3authInitialState: IWeb3AuthState | undefined;
+}) {
+  // IMP END - SSR
   return (
     // IMP START - Setup Web3Auth Provider
     // IMP START - SSR
@@ -46,7 +62,6 @@ export default function Provider({ children, web3authInitialState }:
         {children}
         </WagmiProvider>
       </QueryClientProvider>
-      {/*// IMP END - Setup Wagmi Provider */}
       {/*// IMP START - Setup Web3Auth Provider */}
     </Web3AuthProvider>
     // IMP END - Setup Web3Auth Provider
